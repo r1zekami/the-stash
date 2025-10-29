b0fe5a920b800b735b8357dd10d1a7ce352df01d
r1zekami <alirurpha@gmail.com>
2025-10-29 17:26:42 +1000

    k8s

--- ctd-k8s.sh
+++ ctd-k8s.sh
@@ -62,7 +62,6 @@ sudo systemctl enable kubelet
 sudo mkdir -p /usr/lib/cni
 curl -L https://github.com/containernetworking/plugins/releases/download/v1.4.0/cni-plugins-linux-amd64-v1.4.0.tgz | sudo tar -C /usr/lib/cni -xz
 
-sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --image-repository=registry.aliyuncs.com/google_containers --v=3
-
-kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
-kubectl taint nodes --all node-role.kubernetes.io/control-plane-
+#sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --image-repository=registry.aliyuncs.com/google_containers --v=3
+#kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
+#kubectl taint nodes --all node-role.kubernetes.io/control-plane-
