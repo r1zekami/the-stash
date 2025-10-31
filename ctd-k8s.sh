e68805f60a28ea017695a47245c0ea2dd2d91890
r1zekami <110971527+r1zekami@users.noreply.github.com>
2025-10-31 14:44:40 +1000

    Update ctd-k8s.sh

--- ctd-k8s.sh
+++ ctd-k8s.sh
@@ -63,7 +63,7 @@ sudo systemctl enable kubelet
 sudo mkdir -p /opt/cni/bin
 sudo mkdir -p /usr/lib/cni
 curl -L https://github.com/containernetworking/plugins/releases/download/v1.7.1/cni-plugins-linux-amd64-v1.7.1.tgz | sudo tar -C /opt/cni/bin -xz
-sudo cp /opt/cni/bin/* /usr/lib/cni/
+sudo ln -s /opt/cni/bin /usr/lib/cni
 
 #sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --image-repository=registry.aliyuncs.com/google_containers --v=3
 #kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
